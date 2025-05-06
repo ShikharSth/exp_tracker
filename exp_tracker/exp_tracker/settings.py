1b541cc363db88cae33ed87ce600212c663589ac
Aakriti Rai <aakritip512@gmail.com>
2025-05-06 15:51:38 +0545

    update on the settings.py file

--- exp_tracker/exp_tracker/settings.py
+++ exp_tracker/exp_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4%2dc+_pz=j33v3@#vgpdo^7&j#m830x8kj@6+1u7b2_no1+2=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
